a31b4a54369832169ce3192a1cced63918fae936
Daniel71230 <133103434+Daniel71230@users.noreply.github.com>
2023-08-29 15:36:32 +0300

    commited

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,17 @@
 function formSbmt(){
     alert("Email sent!");
 }
+
+function openMenu(){
+    var menu = document.getElementsByClassName("overlay-menu")[0];
+    var cross = document.getElementsByClassName("nav-menu-open")[0];
+    if(cross.classList.contains('opened')){
+	    menu.style.display = "none";
+        cross.classList.remove('opened');
+    }
+    else{
+        menu.style.display = "block";
+        cross.classList.add('opened');
+    }
+}
+    
